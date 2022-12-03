6d8858434f4804fad471521877a02242227063be
Diogo Silva Dorta <101647390+diogoodorta@users.noreply.github.com>
2022-12-03 13:54:34 -0300

    4 atualização
    fazendo uma movimentação em primeira pessoa

--- Evil Maze/Assets/scripts/script personagem/PlayerControler.cs
+++ Evil Maze/Assets/scripts/script personagem/PlayerControler.cs
@@ -11,6 +11,10 @@ public class PlayerControler : MonoBehaviour
     [SerializeField] private float rY;
     [SerializeField] private float rX;
 
+    
+    [SerializeField] private Transform camerapivor;
+    [SerializeField] private Transform camera;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +30,9 @@ public class PlayerControler : MonoBehaviour
         rY = Mathf.Clamp(rY - (Input.GetAxisRaw("Mouse Y") * 2 * 100 * Time.deltaTime), -30, 30);
 
         player.Rotate(0, rX, 0, Space.World);
+        camera.rotation = Quaternion.Lerp(camera.rotation, Quaternion.Euler(rY * 2, player.eulerAngles.y, 0), 100 * Time.deltaTime);
+
+        camerapivor.position = Vector3.Lerp(camerapivor.position, player.position, 10 * Time.deltaTime);
     }
 
     private void FixedUpdate()
