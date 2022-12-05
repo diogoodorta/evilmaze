8eee1e0a7319a98dd4dd69424015c878200b9ea0
Diogo Silva Dorta <101647390+diogoodorta@users.noreply.github.com>
2022-12-04 22:17:23 -0300

    ssss

--- Evil Maze/Assets/scripts/script do menu/Menu.cs
+++ Evil Maze/Assets/scripts/script do menu/Menu.cs
@@ -5,9 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {    
-    public void LoadScene(string cena)
+    public void LoadScene(string fase)
     {
-       SceneManager.LoadScene(cena);
+       SceneManager.LoadScene(fase);
     }
 
 
